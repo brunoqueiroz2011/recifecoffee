8179715bf49c09dae30656a240975edad7d55c19
Bruno Gonçalves de Queiroz <bruno.queiroz2011@gmail.com>
2018-05-24 21:26:40 -0300

    Criei o layout do login, inclusão dos icones e imagens

--- app/src/main/java/br/com/agnes_bruno/recifecoffee/LoginActivity.java
+++ app/src/main/java/br/com/agnes_bruno/recifecoffee/LoginActivity.java
@@ -13,8 +13,7 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
