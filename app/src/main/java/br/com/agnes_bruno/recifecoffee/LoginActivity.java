b43272115995bacc4e58eb678f4f0598436990c3
Bruno Gonçalves de Queiroz <bruno.queiroz2011@gmail.com>
2018-05-24 23:53:42 -0300

    Ajustes na tela de Login, inclusão da logo e criação da SharedPreference para apresentar a SplashScreen apenas na primeira vez que o app é aberto

--- app/src/main/java/br/com/agnes_bruno/recifecoffee/LoginActivity.java
+++ app/src/main/java/br/com/agnes_bruno/recifecoffee/LoginActivity.java
@@ -1,11 +1,13 @@
 package br.com.agnes_bruno.recifecoffee;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -23,6 +25,20 @@ public class LoginActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        Button btn_Login = findViewById(R.id.btn_Login);
+        btn_Login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mostrarLogin();
+            }
+        });
+    }
+    private void mostrarLogin() {
+        Intent intent = new Intent(LoginActivity.this,
+                SplashScreenActivity.class);
+        startActivity(intent);
+        finish();
     }
 
 }
--- app/src/main/java/br/com/agnes_bruno/recifecoffee/SplashScreenActivity.java
+++ app/src/main/java/br/com/agnes_bruno/recifecoffee/SplashScreenActivity.java
@@ -1,6 +1,7 @@
 package br.com.agnes_bruno.recifecoffee;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -12,8 +13,23 @@ public class SplashScreenActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+        SharedPreferences preferences =
+                getSharedPreferences("user_preferences", MODE_PRIVATE);
+        if (preferences.contains("is_open_app")) {
+            mostrarLogin();
+        } else {
+            adicionarPreferenceJaAbriu(preferences);
+            mostrarSplash();
+        }
+    }
 
+    private void adicionarPreferenceJaAbriu(SharedPreferences preferences) {
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.putBoolean("is_open_app", true);
+        editor.commit();
+    }
 
+    private void mostrarSplash() {
         Handler handle = new Handler();
         handle.postDelayed(new Runnable() {
             @Override
